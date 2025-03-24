3cdf572f7494dbddca648c3aa157a28d456d2654
Senthilkumar <senthilkumar@Senthilkumars-MacBook-Air.local>
2025-03-24 13:20:30 +0530

    Remove DB info

--- back-end/src/config/database.js
+++ back-end/src/config/database.js
@@ -2,7 +2,7 @@ const { default: mongoose } = require("mongoose");
 const { DATA_BASE_STR } = require("../constant");
 
 const connectDB = async () => {
-  await mongoose.connect(DATA_BASE_STR);
+  await mongoose.connect(process.env.DATA_BASE_STR);
 };
 
 module.exports = {
--- back-end/src/constant.js
+++ back-end/src/constant.js
@@ -1,3 +1,1 @@
-const DATA_BASE_STR =
-  "mongodb+srv://senthil:senthil_85@nodeapi.cypfr.mongodb.net/devtinder";
-module.exports = { DATA_BASE_STR };
+
